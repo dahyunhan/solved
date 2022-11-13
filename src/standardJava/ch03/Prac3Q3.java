3d0c109d713e8813e9e4eb3357e3d3ca58eb50b3
dollae <dollaeboss@gmail.com>
2022-11-13 21:38:37 +0900

    220626

---
+++ src/standardJava/ch03/Prac3Q3.java
@@ -0,0 +1,9 @@
+package standardJava.ch03;
+
+public class Prac3Q3 {
+
+    public static void main(String[] args) {
+        int num = 111;
+        System.out.println(num - (num % 100));
+    }
+}
